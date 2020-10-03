728e6e9c2216de6274c805cdad144a9689472815
aaron3993 <aaron3993@hotmail.com>
2020-10-03 13:30:01 -0400

    Successfully implemented communication through console.logs between the clients

--- server.js
+++ server.js
@@ -7,12 +7,24 @@ const socket = require('socket.io')
 const io = socket(server)
 const PORT = 8080
 
+// const videoSocketId
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-io.on('connection', socket => {
+io.on('connect', socket => {
   console.log('client connected')
+  io.emit('welcome')
+
+  socket.on('hi', () => {
+    console.log('controls says hi to server')
+    io.emit('hiPlayer')
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 })
 
 server.listen(PORT, () => console.log(`server is running on port ${PORT}`))
