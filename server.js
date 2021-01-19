3fb0424b5dd56bfc8d8c6eeed8a65f688d4978b2
Aaron <aaron3993@hotmail.com>
2021-01-18 20:25:37 -0500

    Changed port for deployment

--- server.js
+++ server.js
@@ -19,4 +19,4 @@ io.on("connect", (socket) => {
   });
 });
 
-server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+server.listen(process.env.PORT || PORT, () => console.log(`server is running on port ${PORT}`));
